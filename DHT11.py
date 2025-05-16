7dd3edcc73f677d6776d226aaa8046948a08649f
Pfo777 <202496495@collegeahuntsic.qc.ca>
2025-05-15 20:10:54 -0400

    DHT11 v.13
    ajout de format JSON pour l'information

--- DHT11.py
+++ DHT11.py
@@ -4,14 +4,20 @@
 import time
 import board
 import adafruit_dht
+from datetime import datetime
 
 dht = adafruit_dht.DHT11(board.D24)  # ##utiliser le bon GPIO
 
 while True:
     try:
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         temperature = dht.temperature
         humidity = dht.humidity
-        print(f"Température: {temperature}°C  Humidité: {humidity}%")
+
+        json_dht11 = {"time":now, "temperature":temperature, "humidity":humidity} #gabarit possible JSON pour POST
+        print(json_dht11)
+
+        print(f"Température: {temperature}°C  Humidité: {humidity}%")  #valider que ça fonctionne
     except RuntimeError as e:
         print(f"Erreur : {e.args[0]}")
     time.sleep(5)
